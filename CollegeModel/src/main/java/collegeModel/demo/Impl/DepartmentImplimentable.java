833330d84fe3b527b10485739838e0472c12e732
Karthik Cherkala Nagaraja <Karthik.Nagaraja@ATMECS.CORP>
2023-05-25 17:49:07 +0530

    Revert "college model project using data structures"
    This reverts commit 4a00eb08a1a17cd149844f42f0847aaad556ba37.

--- CollegeModel/src/main/java/collegeModel/demo/Impl/DepartmentImplimentable.java
+++
@@ -1,9 +0,0 @@
-package collegeModel.demo.Impl;
-
-import java.util.List;
-
-import collegeModel.demo.entities.Student;
-
-public interface DepartmentImplimentable {
-	public int getAllStudentInDepts(List<Student> students,String branchName);
-}
